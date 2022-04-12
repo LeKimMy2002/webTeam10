fee2ecdb21fe0c4e0eb46324dd240d523b5bbda8
minhnguyendev2002 <85899672+minhnguyendev2002@users.noreply.github.com>
2022-04-12 09:42:53 +0700

    Hoàn thành dự án

--- pages/admin/src/index.js
+++ pages/admin/src/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./Components/Navigation/Navigation"
 import reportWebVitals from './reportWebVitals';
@@ -11,25 +10,42 @@ import Member from "./Pages/Member/Member"
 import OrderList from "./Pages/orderList/orderList";
 import MenuList from "./Pages/menuList/menuList"
 import AddPost from "./Pages/addPost/addPost";
+import Login from "./Components/Login/Login";
+import {LoginProvider, LoginContext} from "./hooks/acceptLogin"
+import './index.css';
+
+const Index = () => {
+  const context = useContext(LoginContext)
+  return (
+    <Router>
+      {
+        context.accept ? 
+        <>
+          <div className="d-flex">
+            <Nav />
+              <Routes>
+                <Route path="/" element={<App />} />
+                <Route exact path="/blogs" element={<Blogs />} />
+                <Route path="/blogs/add-post" element={<AddPost />} />
+                <Route path="/customers" element={<Customers />} />
+                <Route path="/member" element={<Member />} />
+                <Route path="/orders-list" element={<OrderList />} />
+                <Route path="/menu-list" element={<MenuList />} />
+                <Route path="/new" element={<MenuList />} />
+              </Routes>
+          </div>
+        </> : <Login />
+      }
+  </Router>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Router>
-    <div className="d-flex">
-      <Nav />
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route exact path="/blogs" element={<Blogs />} />
-          <Route path="/blogs/add-post" element={<AddPost />} />
-          <Route path="/customers" element={<Customers />} />
-          <Route path="/member" element={<Member />} />
-          <Route path="/orders-list" element={<OrderList />} />
-          <Route path="/menu-list" element={<MenuList />} />
-          <Route path="/new" element={<MenuList />} />
-        </Routes>
-    </div>
-  </Router>
+  <LoginProvider>
+    <Index />
+  </LoginProvider>
 );
 
 reportWebVitals();
